0d491ec6d6a6f39c6480d92508269eda4afd3713
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-28 00:41:53 +0000

    Test main method

--- src/test/java/sn/sdley/springtestintegration/SpringTestIntegrationApplicationTests.java
+++ src/test/java/sn/sdley/springtestintegration/SpringTestIntegrationApplicationTests.java
@@ -3,11 +3,20 @@ package sn.sdley.springtestintegration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class SpringTestIntegrationApplicationTests {
 
     @Test
     void contextLoads() {
+        // If the application context fails to load, this test will fail
+    }
+
+    @Test
+    void mainMethodShouldRunWithoutExceptions() {
+        // Calling the main() method and ensuring no exception occurs
+        assertDoesNotThrow(() -> SpringTestIntegrationApplication.main(new String[]{}));
     }
 
 }
